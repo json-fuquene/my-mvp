83d4d34ec20f01b38c1b7e495e2f7d37a49febe0
jfuquene <fuquene.corp@gmail.com>
2026-03-12 00:00:02 -0500

    Configuracion cliente prisma

---
+++ src/lib/prisma.js
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client'
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
+
+const adapter = new PrismaBetterSqlite3({
+  url: process.env.DATABASE_URL
+})
+
+const globalForPrisma = globalThis
+
+const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
+
+export default prisma
